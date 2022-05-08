e8934a15625cad3952c0c563dc01c36cfd4b287c
liu <liunian@2077tech.com>
2022-05-08 16:44:05 +0800

    添加 tailwind 和 sass

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -1,4 +1,12 @@
 declare module "*.css" {
-    const mapping: Record<string, string>;
-    export default mapping;
+  const mapping: Record<string, string>;
+  export default mapping;
+}
+declare module "*.scss" {
+  const mapping: Record<string, string>;
+  export default mapping;
+}
+declare module "*.sass" {
+  const mapping: Record<string, string>;
+  export default mapping;
 }
